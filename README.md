359ce81770939e4b4f6fb751cb7971ff9780656d
Extav <106775295+whohensee@users.noreply.github.com>
2023-07-04 08:52:06 -0700

    Create functions for basic math

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # calculator
+
+### Stage 1 ###
+
+Create the following functions:
+
+add(a,b)
+subtract(a,b)
+multiply(a,b)
+divide(a,b)
+
--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 "use strict";
+
+// ----- Basic Computation Functions ----- //
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+// ----- Testing Area ----- //
+console.log(divide(5.2, 10));
