6423054231de07794c13be75e2fdd7690d8adbd4
Extav <106775295+whohensee@users.noreply.github.com>
2023-07-04 09:07:35 -0700

    Add operate() which calls a function to eval input
      The idea is that operate() can be called to evaluate whatever has
    been input to the calculator so far. I have some ideas as for how it
    could behave recursively, sort of like such:
    
    if LHS and RHS are num, call one of the functions on them
    
    if either LHS or RHS are string, call operate on them individually,
      reducing them.
    
      This requires some sort of string parsing option at the start of
    operate which I will get to soon.

--- script.js
+++ script.js
@@ -24,5 +24,24 @@ let calcLHS = null;
 let calcRHS = null;
 let calcOperator = null;
 
+function operate(lhs, rhs, operator) {
+  let result = null;
+  switch (operator) {
+    case "+":
+      result = add(lhs, rhs);
+      break;
+    case "-":
+      result = subtract(lhs, rhs);
+      break;
+    case "*":
+      result = multiply(lhs, rhs);
+      break;
+    case "/":
+      result = divide(lhs, rhs);
+      break;
+  }
+  return result;
+}
+
 // ----- Testing Area ----- //
-console.log(divide(5.2, 10));
+console.log(operate(15,6, "/"));
