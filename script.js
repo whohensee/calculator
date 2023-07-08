bf3044e311499bbc5c86c4fe5e3bcc747a6d77ff
Extav <106775295+whohensee@users.noreply.github.com>
2023-07-08 08:36:03 -0700

    Improve display functions and default behavior

--- script.js
+++ script.js
@@ -24,6 +24,8 @@ let calcLHS = null;
 let calcRHS = null;
 let calcOperator = null;
 let displayStr = "";
+const displayDefaultStr = "lets calculate";
+const display = document.querySelector("#calc-display");
 
 function operate(lhs, rhs, operator) {
   let result = null;
@@ -45,13 +47,19 @@ function operate(lhs, rhs, operator) {
 }
 
 function addToDisplay(str) {
-  if (typeof str === "string") {
-    displayStr = displayStr.concat(str);
-  } else {
+  if (typeof str !== "string") {
     console.log("ERROR IN addToDisplay");
     return null;
   }
+
+  if (displayStr === displayDefaultStr) {
+    displayStr = "";
+  }
+
+  displayStr = displayStr.concat(str);
+  display.textContent = displayStr;
 }
 
 // ----- Testing Area ----- //
 // console.log(operate(15, 6, "/"));
+console.log(display.textContent);
